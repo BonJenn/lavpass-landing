04af35c1ed4f24fe17360e73ccdfe5f1521286da
Jonathan Benn <bonjennprojects@gmail.com>
2025-02-19 17:47:59 -0800

    Changed font and modernized design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Lexend } from '@next/font/google';
+import { Inter, Lexend } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
 const lexend = Lexend({
-  weight: ['400', '700'],
-  subsets: ['latin'],
+  weight: ["400", "700"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -25,14 +19,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${lexend.className} antialiased`}
-      >
+      <body className={`${inter.variable} ${lexend.className} antialiased`}>
         {children}
       </body>
     </html>
