70e6d70c493b9aaf8eea88310323312e0a91af90
Jonathan Benn <bonjennprojects@gmail.com>
2025-04-09 14:57:37 -0700

    Update Facebook and YouTube links in footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -559,7 +559,7 @@ export default function Home() {
           
           <div className="flex justify-center space-x-6 mb-8">
             {/* Social Media Icons */}
-            <a href="#" className="hover:opacity-80 transition transform hover:scale-110 cursor-pointer">
+            <a href="https://www.facebook.com/profile.php?id=61574902000865&mibextid=wwXIfr&mibextid=wwXIfr" className="hover:opacity-80 transition transform hover:scale-110 cursor-pointer">
               {/* Facebook */}
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white">
                 <path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z" />
@@ -579,7 +579,7 @@ export default function Home() {
                 <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z" />
               </svg>
             </a>
-            <a href="#" className="hover:opacity-80 transition transform hover:scale-110 cursor-pointer">
+            <a href="https://youtube.com/@lavpass?si=HHjMdvlrevd4cMrI" className="hover:opacity-80 transition transform hover:scale-110 cursor-pointer">
               {/* YouTube */}
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white">
                 <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
