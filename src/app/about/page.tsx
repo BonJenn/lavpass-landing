db884ccd37373f1323a18de13e945bd12317b991
Jonathan Benn <bonjennprojects@gmail.com>
2025-06-01 14:57:54 -0700

    Fixed header and footer on mobile

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,7 @@ export default function About() {
 
       {/* HEADER */}
       <header className="fixed top-0 left-0 right-0 z-50 bg-black/20 backdrop-blur-md border-b border-white/10">
-        <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex justify-center md:justify-between items-center">
           <Link href="/">
             <Image src="/images/lavpass_logo_white.png" alt="LavPass Logo" width={160} height={64} className="cursor-pointer" />
           </Link>
@@ -108,6 +108,15 @@ export default function About() {
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
                   </svg>
+                </a>
+                <a href="https://www.snapchat.com/add/lavpass?sender_web_id=133102ee-99ce-4257-bc85-aeab12bf9f8f&device_type=desktop&is_copy_url=true" className="text-blue-100 hover:text-white transition">
+                  <Image
+                    src="/images/snapchat_logo.png"
+                    alt="Snapchat"
+                    width={24}
+                    height={24}
+                    className="opacity-80 hover:opacity-100 transition-opacity"
+                  />
                 </a>
               </div>
             </div>
